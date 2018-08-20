b45f1cda86e18c85a1d75ea50a60c5d009faec63
Troy896 <troy896@gmail.com>
2018-08-20 09:50:40 -0500

    Hekate ipl and submodule updates

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#Build script for PatNX
+#Simple build script to compile, clean and copy over to sd files.
+
+mkdir build && cd build
+mkdir atmosphere
+mkdir atmosphere/titles
+mkdir atmosphere/titles/0100000000000036
+mkdir atmosphere/titles/0100000000000036/exefs
+mkdir atmosphere/titles/010000000000100D
+mkdir atmosphere/titles/010000000000100D/exefs
+mkdir atmosphere/exefs_patches
+mkdir PatNX
+mkdir switch
+mkdir ftpd
+cd ..
