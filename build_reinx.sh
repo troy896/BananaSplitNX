9b586b29e18d824a37d120e7f46b3badb4ea836b
Troy896 <troy896@gmail.com>
2018-08-21 00:27:05 -0500

    finished Reinx build script

--- build_reinx.sh
+++ build_reinx.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-#Build script for PatNX
+#Build script for BananaSplitNX
 #Simple build script to compile, clean and copy over to sd files.
 
 mkdir build 
 cd build
-mkdir atmosphere
-mkdir atmosphere/titles
-mkdir atmosphere/titles/0100000000000036
-mkdir atmosphere/titles/0100000000000036/exefs
-mkdir atmosphere/titles/010000000000100D
-mkdir atmosphere/titles/010000000000100D/exefs
-mkdir atmosphere/exefs_patches
-mkdir PatNX
 mkdir switch
 mkdir switch/tinfoil
 mkdir switch/NX-Shell
@@ -20,52 +12,44 @@ mkdir switch/Checkpoint
 mkdir tinfoil
 mkdir tinfoil/nsp
 mkdir ftpd
-cp /mnt/d/Github/PatNX/hekate_ipl.ini /mnt/d/Github/PatNX/build/hekate_ipl.ini
-cp /mnt/d/Github/PatNX/bootlogo.bmp /mnt/d/Github/PatNX/build/bootlogo.bmp
-cp /mnt/d/Github/PatNX/NX-Shell/NX-Shell.nro /mnt/d/Github/PatNX/build/switch/NX-Shell/NX-Shell.nro
-cd /mnt/d/Github/PatNX/hekate
+
+cp /mnt/d/Github/BananaSplitNX/NX-Shell/NX-Shell.nro /mnt/d/Github/BananaSplitNX/build/switch/NX-Shell/NX-Shell.nro
+cd /mnt/d/Github/BananaSplitNX/ReiNX
 make
-cp /mnt/d/Github/PatNX/hekate/output/hekate.bin /mnt/d/Github/PatNX/build/PatNX.bin
+cp /mnt/d/Github/BananaSplitNX/ReiNX/out/ReiNX.bin /mnt/d/Github/BananaSplitNX/build/BananaSplitNX.bin
+cp -r "/mnt/d/Github/BananaSplitNX/ReiNX/out/ReiNX" /mnt/d/Github/BananaSplitNX/build
+cd /mnt/d/Github/BananaSplitNX/build/ReiNX
+mkdir titles
+mkdir titles/010000000000100D
+mkdir titles/010000000000100D/exefs
+cd /mnt/d/Github/BananaSplitNX/ReiNX
 make clean
-cp /mnt/d/Github/PatNX/atmosphere.patch /mnt/d/Github/PatNX/Atmosphere/atmosphere.patch
-cd /mnt/d/Github/PatNX/Atmosphere
-patch -p1 --binary < atmosphere.patch
-rm atmosphere.patch
+cd /mnt/d/Github/BananaSplitNX/sys-ftpd
 make
-cp /mnt/d/Github/PatNX/Atmosphere/stratosphere/creport/creport.nso /mnt/d/Github/PatNX/build/atmosphere/titles/0100000000000036/exefs/main
-cp /mnt/d/Github/PatNX/Atmosphere/stratosphere/creport/creport.npdm /mnt/d/Github/PatNX/build/atmosphere/titles/0100000000000036/exefs/main.npdm
-cp /mnt/d/Github/PatNX/Atmosphere/stratosphere/loader/loader.kip /mnt/d/Github/PatNX/build/PatNX/loader.kip
-cp /mnt/d/Github/PatNX/Atmosphere/stratosphere/fs_mitm/fs_mitm.kip /mnt/d/Github/PatNX/build/PatNX/fs_mitm.kip
-cp /mnt/d/Github/PatNX/Atmosphere/stratosphere/pm/pm.kip /mnt/d/Github/PatNX/build/PatNX/pm.kip
-cp /mnt/d/Github/PatNX/Atmosphere/stratosphere/sm/sm.kip /mnt/d/Github/PatNX/build/PatNX/sm.kip
-cp -r "/mnt/d/Github/PatNX/Fake Tickets" /mnt/d/Github/PatNX/build/atmosphere/exefs_patches
+cp /mnt/d/Github/BananaSplitNX/sys-ftpd/sys-ftpd.kip /mnt/d/Github/BananaSplitNX/build/ReiNX/sysmodules/sys_ftpd.kip
+cp /mnt/d/Github/BananaSplitNX/sys-ftpd/sd_card/ftpd/* /mnt/d/Github/BananaSplitNX/build/ftpd/
 make clean
-cd /mnt/d/Github/PatNX/sys-ftpd
-make
-cp /mnt/d/Github/PatNX/sys-ftpd/sys-ftpd.kip /mnt/d/Github/PatNX/build/PatNX/sys_ftpd.kip
-cp /mnt/d/Github/PatNX/sys-ftpd/sd_card/ftpd/* /mnt/d/Github/PatNX/build/ftpd/
-make clean
-cd /mnt/d/Github/PatNX/nx-hbmenu
+cd /mnt/d/Github/BananaSplitNX/nx-hbmenu
 make nx
-cp /mnt/d/Github/PatNX/nx-hbmenu/nx-hbmenu.nro /mnt/d/Github/PatNX/build/hbmenu.nro
+cp /mnt/d/Github/BananaSplitNX/nx-hbmenu/nx-hbmenu.nro /mnt/d/Github/BananaSplitNX/build/hbmenu.nro
 make clean
-cd /mnt/d/Github/PatNX/nx-hbloader
+cd /mnt/d/Github/BananaSplitNX/nx-hbloader
 make
-cp /mnt/d/Github/PatNX/nx-hbloader/hbl.nso /mnt/d/Github/PatNX/build/atmosphere/titles/010000000000100D/exefs/main
-cp /mnt/d/Github/PatNX/nx-hbloader/hbl.npdm /mnt/d/Github/PatNX/build/atmosphere/titles/010000000000100D/exefs/main.npdm
-touch /mnt/d/Github/PatNX/build/atmosphere/titles/010000000000100D/exefs/rtld.stub
+cp /mnt/d/Github/BananaSplitNX/nx-hbloader/hbl.nso /mnt/d/Github/BananaSplitNX/build/ReiNX/titles/010000000000100D/exefs/main
+cp /mnt/d/Github/BananaSplitNX/nx-hbloader/hbl.npdm /mnt/d/Github/BananaSplitNX/build/ReiNX/titles/010000000000100D/exefs/main.npdm
+touch /mnt/d/Github/BananaSplitNX/build/ReiNX/titles/010000000000100D/exefs/rtld.stub
 make clean
-cd /mnt/d/Github/PatNX/Tinfoil
+cd /mnt/d/Github/BananaSplitNX/Tinfoil
 make
-cp /mnt/d/Github/PatNX/Tinfoil/Tinfoil.nro /mnt/d/Github/PatNX/build/switch/tinfoil/tinfoil.nro
+cp /mnt/d/Github/BananaSplitNX/Tinfoil/Tinfoil.nro /mnt/d/Github/BananaSplitNX/build/switch/tinfoil/tinfoil.nro
 make clean
-cd /mnt/d/Github/PatNX/Checkpoint
+cd /mnt/d/Github/BananaSplitNX/Checkpoint
 make
-cp /mnt/d/Github/PatNX/Checkpoint/switch/out/Checkpoint.nro /mnt/d/Github/PatNX/build/switch/Checkpoint/Checkpoint.nro
+cp /mnt/d/Github/BananaSplitNX/Checkpoint/switch/out/Checkpoint.nro /mnt/d/Github/BananaSplitNX/build/switch/Checkpoint/Checkpoint.nro
 make clean
-cd /mnt/d/Github/PatNX/build
+cd /mnt/d/Github/BananaSplitNX/build
 export DATE=$(date +%s | cut -c 1-8)
-zip -r9 PatNX-$DATE.zip .
-cp /mnt/d/Github/PatNX/build/PatNX-$DATE.zip /mnt/d/Github/PatNX/out/PatNX-$DATE.zip
-cd /mnt/d/Github/PatNX
+zip -r9 BananaSplitNX-$DATE.zip .
+cp /mnt/d/Github/BananaSplitNX/build/BananaSplitNX-$DATE.zip /mnt/d/Github/BananaSplitNX/out/BananaSplitNX-$DATE.zip
+cd /mnt/d/Github/BananaSplitNX
 rm -r build
